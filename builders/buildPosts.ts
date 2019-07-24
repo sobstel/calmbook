8bbc7f523f5d0c7ccb39ea0519fb2fe2fadcb62d
malczak <mateusz@malczak.info>
2019-07-24 13:35:59 +0200

    removes console.log 😨

--- builders/buildPosts.ts
+++ builders/buildPosts.ts
@@ -4,11 +4,8 @@ import buildPost from "../builders/buildPost";
 
 const buildPosts = ($: CheerioSelector): Post[] => {
   const posts: Post[] = [];
-  console.log(($ as any).html());
-
   $(".userContentWrapper").each((_, element) => {
     const post = buildPost(cheerio.load(element));
-    console.log(post);
     posts.push(post);
   });
 
