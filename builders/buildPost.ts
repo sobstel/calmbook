19941ade454ae4be94970a0e7614cb87eeb6c173
malczak <mateusz@malczak.info>
2019-07-24 12:07:28 +0200

    cleans post text from html tags

--- builders/buildPost.ts
+++ builders/buildPost.ts
@@ -3,7 +3,12 @@ import { Post } from "../models";
 const buildPost = ($: CheerioSelector): Post => {
   const timestamp = parseInt($("abbr[data-utime]").attr("data-utime")) * 1000;
   const { message, images = [] } = buildContent($);
-  const title = message.replace(/<[^>]+>/g, "").split(" ").slice(0, 8).join(" ") + "..."
+  const title =
+    message
+      .replace(/<[^>]+>/g, "")
+      .split(" ")
+      .slice(0, 8)
+      .join(" ") + "...";
 
   return { title, timestamp, message, images };
 };
@@ -14,14 +19,23 @@ const buildContent = (
   $: CheerioSelector
 ): { message: string; images: string[] } => {
   const userContent = $(".userContent");
-  let message = $("p", userContent).html() || "";
 
-  const textExposedRootPargaraph = $(".text_exposed_root > p");
-  if (textExposedRootPargaraph.length) {
-    const textExposedShow = $("div.text_exposed_show").html() || "";
-    message = `<p>${textExposedRootPargaraph.html()}</p>${textExposedShow}`;
-  }
+  // get all post paragraphs post text
+  const paragraphs = $("p", userContent).toArray();
 
+  // remove html leaving only <br>
+  let message = paragraphs.reduce(
+    (accum: string, el: CheerioElement): string => {
+      $(el)
+        .find("br")
+        .replaceWith("\n");
+      accum = `${accum}<p>${$(el).text()}</p>`.replace(/\n+/g, "<br/>");
+      return accum;
+    },
+    ""
+  );
+
+  // fetch any post images
   const imgNodes = userContent.next().find("img.img");
   const images =
     imgNodes && imgNodes.toArray().map(element => element.attribs["src"]);
