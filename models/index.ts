a9a0b840aa2d9c323e9608cdd098e0f8bff8ef77
malczak <mateusz@malczak.info>
2019-07-25 17:49:55 +0200

    adds post link detection

--- models/index.ts
+++ models/index.ts
@@ -1,9 +1,24 @@
+export const enum PostType {
+  text = "text",
+  image = "image",
+  video = "video",
+  link = "link",
+  gallery = "gallery",
+  complex = "complex"
+}
+
+export interface PostLink {
+  excerpt: string;
+  url: string;
+}
+
 export interface Post {
   id: string;
   images: string[];
   message: string;
   timestamp: number;
   title: string;
+  link?: PostLink;
 }
 
 export interface Page {
