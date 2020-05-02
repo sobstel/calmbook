926d0c21ac3089d84f65dcedc7033ae41e26e671
Przemek Sobstel <przemek@sobstel.org>
2020-05-02 17:10:14 +0200

    Generate xml feed

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,6 +1,8 @@
 import { NextPageContext } from "next";
 import useSWR from "swr";
 import Page from "components/Page";
+import { json } from "util/request";
+import generateFeed from "util/generateFeed";
 
 type Props = {
   // data: { page: Page };
@@ -11,11 +13,32 @@ function apiPath(slug: string): string {
   return `/api/page?name=${slug}`;
 }
 
-export async function getServerSideProps({ query }: NextPageContext) {
-  const slug = query.slug as string;
-  // const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-  // const url = `${protocol}://${req?.headers.host}${apiPath(slug)}`;
-  // const data = await request(url);
+// TODO: move to "util"
+const sanitizeSlug = (name: string): string => {
+  let sanitizedSlug = name;
+  sanitizedSlug = sanitizedSlug.trim();
+  sanitizedSlug = sanitizedSlug.replace(/\/+$/g, ""); // trailing slashes
+  sanitizedSlug = sanitizedSlug.replace(/^\/+/g, ""); // leading slashes
+  sanitizedSlug = sanitizedSlug.replace(/\.\w{3}$/, ""); // extension (format)
+  sanitizedSlug = sanitizedSlug.replace(/#.+$/, ""); // fragment link
+  return sanitizedSlug;
+};
+
+export async function getServerSideProps({ query, req, res }: NextPageContext) {
+  const rawSlug = query.slug as string;
+  const slug = sanitizeSlug(rawSlug);
+
+  if (req && res && rawSlug.slice(-4) === ".xml") {
+    const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+    const url = `${protocol}://${req.headers.host}${apiPath(slug)}`;
+    const data = await json(url);
+
+    res.setHeader("Content-Type", "text/xml");
+    res.write(generateFeed(data.page));
+    res.end();
+    return;
+  }
+
   return { props: { slug } };
 }
 
