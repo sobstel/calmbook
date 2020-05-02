591647df18eb7ee38e2e59a59f250ad3b82f00c9
Przemek Sobstel <przemek@sobstel.org>
2020-05-03 00:30:21 +0200

    Page loading skeleton (#26)
    * Bring back avatar/name for posts
    
    * Encode URI before sending request
    
    * Add loading skeleton, spinner and fade-in animations

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,33 +1,58 @@
 import Post from "./Post";
 import Logo from "./Logo";
+import Spinner from "./Spinner";
+import { useSpring, animated } from "react-spring";
 
 type Props = { page: Page };
 
-export default function Page({ page }: Props) {
-  if (!page) {
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
+function Skeleton() {
   return (
     <div>
+      <div className="flex flex-col items-center justify-center">
+        <div
+          className="flex items-center justify-center bg-gray-300 w-16 h-16 my-2"
+          style={{ borderRadius: 32 }}
+        >
+          <Spinner />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function Page({ page }: Props) {
+  if (!page) return <Skeleton />;
+
+  const pageSpring = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+  });
+
+  const postsSpring = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    delay: 500,
+  });
+
+  return (
+    <animated.div style={pageSpring}>
       <img
         className="w-16 h-16 rounded-full m-auto my-2"
         src={page.avatar}
         alt={page.name}
       />
       <h1 className="text-3xl font-bold text-center">{page.name}</h1>
-      <div className="my-8 divide-y divide-gray-300 border-t border-b border-gray-300">
+      <animated.div
+        style={postsSpring}
+        className="my-8 divide-y divide-gray-300 border-t border-b border-gray-300"
+      >
         {page.posts.map((post) => (
           <div key={post.timestamp} className="py-8">
             <Post post={post} page={page} />
           </div>
         ))}
-      </div>
+      </animated.div>
       <Logo />
-    </div>
+    </animated.div>
   );
 }
--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -6,13 +6,21 @@ type Props = { post: Post; page: Page };
 export default function Post({ post, page }: Props) {
   return (
     <div id={post.timestamp.toString()} className="px-2">
-      <div className="mb-4">
-        <a
-          href={`/${page.url}#${post.timestamp}`}
-          className="px-2 py-1 font-medium text-sm bg-gray-300 hover:bg-blue-100 rounded"
-        >
-          {dayjs(post.timestamp).format("YYYY-MM-DD HH:mm")}
-        </a>
+      <div className="flex items-center max-w-full mb-4">
+        <img className="w-10 h-10 rounded-full mr-4" src={page.avatar} alt="" />
+        <div className="text-sm flex-grow inline-block truncate">
+          <p className="text-gray-900 leading-none font-semibold text-lg truncate">
+            {page.name} assasdadsdaad
+          </p>
+          <div className="text-gray-600">
+            <a
+              href={`/${page.url}#${post.timestamp}`}
+              className="hover:underline"
+            >
+              {dayjs(post.timestamp).format("YYYY-MM-DD HH:mm")}
+            </a>
+          </div>
+        </div>
       </div>
 
       <div
