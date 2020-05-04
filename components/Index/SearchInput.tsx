469e722f750ef74605356c26591720a7df3c4bc2
Przemek Sobstel <przemek@sobstel.org>
2020-05-04 11:38:28 +0200

    Change SearchInput to be controlled

--- components/Index/SearchInput.tsx
+++ components/Index/SearchInput.tsx
@@ -1,29 +1,53 @@
-export default function SearchInput() {
+// import { useRef } from "react";
+import SearchIcon from "./SearchIcon";
+import Spinner from "../Spinner";
+
+type Props = {
+  value: string;
+  loading: boolean;
+  onChange: (value: string) => void;
+  onSubmit: () => void;
+};
+
+export default function SearchInput({
+  value,
+  loading,
+  onChange,
+  onSubmit,
+}: Props) {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    onChange(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onSubmit();
+  };
+
   return (
     <div className="relative text-lg bg-transparent text-gray-800">
-      <div className="flex items-center border-b border-b-2 border-gray-500 py-2">
-        <input
-          autoFocus
-          className="bg-transparent border-none mr-3 px-2 leading-tight focus:outline-none font-medium"
-          type="text"
-        />
-        <button type="submit" className="absolute right-0 top-0 mt-3 mr-4">
-          <svg
-            className="h-4 w-4 fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            version="1.1"
-            id="Capa_1"
-            x="0px"
-            y="0px"
-            viewBox="0 0 56.966 56.966"
-            xmlSpace="preserve"
-            width="512px"
-            height="512px"
-          >
-            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-          </svg>
-        </button>
+      <div className="flex items-center border-b border-gray-800 py-2">
+        <form onSubmit={handleSubmit}>
+          <input
+            autoFocus
+            className="bg-transparent border-none mr-3 px-2 pr-5 leading-tight focus:outline-none font-medium"
+            type="text"
+            value={value}
+            onChange={handleChange}
+            disabled={loading}
+          />
+
+          {loading ? (
+            <div className="absolute right-0 top-0 mt-5 mr-4">
+              <Spinner />
+            </div>
+          ) : (
+            <button type="submit" className="absolute right-0 top-0 mt-3 mr-2">
+              <SearchIcon />
+            </button>
+          )}
+        </form>
       </div>
     </div>
   );
--- components/Index/index.tsx
+++ components/Index/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Logo from "../Logo";
 import SearchInput from "./SearchInput";
 import FadeIn from "../FadeIn";
@@ -5,11 +6,34 @@ import FadeIn from "../FadeIn";
 type Props = {};
 
 export default function Index({}: Props) {
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  // const [results, setResults] = useState([]);
+
+  const onChange = (value: string) => setQuery(value);
+  const onSubmit = () => {
+    setLoading(true);
+    // TODO: trigger query (with timeout)
+    // TODO: setResults on success
+    // TODO: setError on error
+    setTimeout(() => setLoading(false), 2000);
+  };
+
+  // TODO: on mount, read from query string
+  // TODO: results
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <FadeIn>
-        <SearchInput />
+        <SearchInput
+          value={query}
+          loading={loading}
+          onChange={onChange}
+          onSubmit={onSubmit}
+        />
+      </FadeIn>
 
+      <FadeIn delay={200}>
         <div className="mt-12">
           <Logo />
         </div>
